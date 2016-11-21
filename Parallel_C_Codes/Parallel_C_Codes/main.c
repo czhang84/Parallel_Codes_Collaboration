6d52fe661256ca319a753f057d9d41daadfcf194
czhang84 <czhang84@hawk.iit.edu>
2016-11-21 14:36:19 -0600

    the starting point of parallel code

---
+++ Parallel_C_Codes/Parallel_C_Codes/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Parallel_C_Codes
+//
+//  Created by zhangcaihua on 11/21/16.
+//  Copyright © 2016 zhangcaihua. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
